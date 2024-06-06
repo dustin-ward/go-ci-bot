7f048a0c5aa7a6267987fab8fd37ecb974afb54e
dustin-ward <dustin.ward@ibm.com>
2024-06-06 13:47:17 -0400

    update script 2024-06-06

--- scripts/gozbot-build-test.sh
+++ scripts/gozbot-build-test.sh
@@ -4,9 +4,8 @@ if [[ -z $2 ]] && ! [[ -z $1 ]]; then
         exit 0
 fi
 
-# Make sure 'cmd/parseGoTest.go' tool is available
-if ! [[ -x "$HOME/parseGoTest" ]]; then
-        echo "parseGoTest executable not found. Halting"
+if ! [[ -x ~/parseGoTest ]]; then
+        echo "No parseGoTest found. Stopping"
         exit 1
 fi
 
@@ -42,18 +41,18 @@ if ! [[ -z "${dir}" ]]; then
 fi
 export TIMEOUTCMD="$HOME/zopen/bin/timeout 7200"
 export CLEANUP="$HOME/zopen/bin/cleanup"
-tagfile=$HOME/zopen/bin/tagfile
 
+tagfile=$HOME/zopen/bin/tagfile
 if ! [[ -x "$CLEANUP" ]]; then
-       export CLEANUP="echo"
-       echo "WARNING cleanup not found"
-       echo "WARNING cleanup not found" >&2
+        export CLEANUP="echo"
+        echo "WARNING cleanup not found"
+        echo "WARNING cleanup not found" >&2
 else
-       $CLEANUP /home/zosgo/gozbot /home/zosgo/tmp /home/zosgo/.cache /tmp
+        $CLEANUP /home/zosgo/go-build /home/zosgo/build1 /home/zosgo/build2 /home/zosgo/build3 /home/zosgo/tmp /home/zosgo/.cache /tmp
 fi
 
 if [[ -r $HOME/.ssh.env ]]; then
-       source $HOME/.ssh.env
+        source $HOME/.ssh.env
 fi
 
 id=${LOGNAME}
@@ -75,11 +74,65 @@ ispid() {
                 false
         fi
 }
-repo=$1
-branch=$2
-build_dir="${HOME}/gozbot/build-${branch}"
+build_dir="${HOME}/go-build"
 mkdir -p $build_dir
 
+if [[ -d $HOME/build1 ]] && [[ -d $HOME/build2 ]] && [[ -d $HOME/build3 ]]; then
+        #on zoscan56 we have workspace pool
+        export NOLOCK=1
+        declare -A pool
+        declare -A dfpool
+        $CLEANUP $HOME/build1 $HOME/build2 $HOME/build3 $HOME/go-build
+
+        pool["$HOME/build1"]="$HOME/build1"
+        pool["$HOME/build2"]="$HOME/build2"
+        pool["$HOME/build3"]="$HOME/build3"
+        pool["$HOME/go-build"]="$HOME"
+
+        while IFS=" " read -r -a line; do
+                dfpool[${line[5]}]=${line[3]}
+        done < <(/bin/df -P -k 2>/dev/null | /bin/tail -n +2)
+
+        max=0
+        res=""
+        for key in ${!pool[@]}; do
+                key1=${pool[$key]}
+                echo "directory $key1 has ${dfpool[$key1]}k free"
+                if [[ ${dfpool[$key1]} -gt $max ]]; then
+                        max=${dfpool[$key1]}
+                        res=$key
+                fi
+        done
+        build_dir="${res}"
+        echo build directory pool selected ${build_dir}
+fi
+if [[ -z "$NOLOCK" ]]; then
+        LOCKDIR=$HOME/lockdir
+        /bin/mkdir "${LOCKDIR}" 2>/dev/null
+        while [[ $? -ne 0 ]]; do
+                opid=$(cat "${LOCKDIR}/pid" 2>/dev/null)
+                ispid "$opid"
+                if [[ $? -eq 0 ]]; then
+                        echo "Waiting for $opid to finish"
+                        sleep 30
+                else
+                        /bin/rm -rf "${LOCKDIR}"
+                fi
+                /bin/mkdir "${LOCKDIR}" 2>/dev/null
+        done
+fi
+trap - USR1
+trap - USR2
+if [[ -z "$NOLOCK" ]]; then
+        trap "rm -f ${LOCKDIR}/pid; rmdir ${LOCKDIR}; exit 0" USR1
+        trap "rm -f ${LOCKDIR}/pid; rmdir ${LOCKDIR}; exit -1" USR2
+        trap "rm -f ${LOCKDIR}/pid; rmdir ${LOCKDIR}; exit" INT TERM EXIT
+        echo $$ >${LOCKDIR}/pid
+else
+        trap "exit 0" USR1
+        trap "exit -1" USR2
+        trap "exit" INT TERM EXIT
+fi
 declare -A cleanlist
 
 tsocmd "delete '${_BPXK_MDUMP}'"
@@ -107,6 +160,21 @@ bootstrap=${GOROOT_BOOTSTRAP:=/home/zosgo/bootstrap}
 die() {
         echo $@
         exit 1
+}
+newdir() {
+        local ndir
+        local prefix="$1"
+        for i in {1..1000}; do
+                if ! [[ -e "${prefix}-$i" ]]; then
+                        ndir="${prefix}-$i"
+                        mkdir -p "${ndir}" || die "cannot create directory ${ndir}"
+                        cleanlist["${ndir}"]=1
+                        echo "${ndir}"
+                        return
+                fi
+        done
+        die "cannot create new directory with prefix $prefix"
+        false
 }
 function timeout() {
         timeout=$1
@@ -154,30 +222,49 @@ mkdir -p $build_dir
 
 cd $build_dir 2>/dev/null || die "failed to cd to $build_dir"
 
-GOCACHE="${build_dir}/gocache"
-mkdir -p $GOCACHE
+repo=$(newdir "$build_dir/build")
+GOCACHE=$(newdir "$build_dir/gocache")
 export GOCACHE
-TMPDIR="$build_dir/tmpdir"
-mkdir -p $TMPDIR
+TMPDIR=$(newdir "$build_dir/tmpdir")
 export TMPDIR
 export GOTOOLCHAIN=local
 
 ! [[ -z "$repo" ]] || die "no suitable repository directory"
 
-if ! [[ -z "$2" ]]; then
-        BR="-b $2"
+if ! [[ -z "$1" ]]; then
+        BR="-b $1"
 else
         unset BR
 fi
 
-git clone --recurse-submodules "git@github.ibm.com:${repo}" $BR build || die "git clone --recurse-submodules git@github.ibm.com:${repo} failed"
+git clone --recurse-submodules git@github.ibm.com:open-z/go.git $BR $repo || die "git clone --recurse-submodules git@github.ibm.com:open-z/go.git failed"
 
-cd ./build || die "cd ./build failed"
+cd $repo || die "cd $repo failed"
 
 git config user.name "build"
 git config user.email "$(id -nu)@$(hostname)"
 
+if [[ -z "$1" ]]; then
+        echo -ne "\nbranch not provided, use default\n\n"
+else
+        if [[ -z "$2" ]]; then
+                git checkout "$1" --recurse-submodule -f || die "git checkout $1 failed"
+        else
+                if [[ "$1" == "$2" ]]; then
+                        git checkout "$1" --recurse-submodule -f || die "git checkout $1 failed"
+                else
+                        git checkout "$2"
+                        git branch -D "$1"
+                        git fetch origin || die "git fetch origin failed"
+                        git checkout --recurse-submodules -f -b "$1" "origin/$1" || die "git checkout -f -b $1 origin/$1 failed"
+                        git merge "origin/$2" -m "Merge-test" || die "git merge $2 failed"
+                fi
+        fi
+fi
+
 branch=$(git status -b --porcelain | head -1 | awk -F / '{print $2}') || die "unknown branch"
+#
+#
 if false; then
         if [[ -r ./VERSION ]]; then
                 buildver=$(/bin/cat ./VERSION)
@@ -186,6 +273,8 @@ if false; then
                 fi
         fi
 fi
+#
+#
 
 version=$(git rev-parse --short HEAD 2>/dev/null)
 git update-index --refresh -q >/dev/null
@@ -228,7 +317,7 @@ export CGO_ENABLED=1
 # retry the build a couple of times
 retries=2
 if [[ "$(type -p storeenv)" != "" ]]; then
-       storeenv >$HOME/.buildenv
+        storeenv >$HOME/.buildenv
 fi
 $tagfile -rq .
 VER=$(cat ./VERSION)
@@ -241,10 +330,22 @@ if [[ $VER =~ ^go1.19 ]]; then
         bootstrap=/home/opnzos/local/1.19.4
 fi
 
+# Determine the proper build script (for backward compat reasons)
+zos_util_dir=misc/zos
+build_script_path=misc/zos/build.sh
+pax_script_path=misc/zos/scripts/pax.sh
+sanity_script_path=misc/zos/test/quicksanity.mak
+if [[ ! -f "misc/zos/build.sh" ]]; then
+        zos_util_dir=go-build-zos
+        build_script_path=go-build-zos/native_build.sh
+        pax_script_path=go-build-zos/native_create_pax.sh
+        sanity_script_path=go-build-zos/quicksanity.mak
+fi
+
 #temp bypass so we can run  the right set of tests.
-#if ! [[ -d go-build-zos ]]; then
-#       git clone git@github.ibm.com:open-z/go-build-zos.git
-#fi
+if ! [[ -d go-build-zos ]]; then
+        git clone git@github.ibm.com:open-z/go-build-zos.git
+fi
 zos_util_dir=go-build-zos
 build_script_path=go-build-zos/native_build.sh
 pax_script_path=go-build-zos/native_create_pax.sh
@@ -306,8 +407,11 @@ $pax_script_path --paxpath="${paxpath}" || die "fail to create ${paxpath}"
 
 cd ..
 
-testdir="$build_dir/test"
-mkdir -p $testdir
+GOCACHE=$(newdir "$build_dir/gocache")
+export GOCACHE
+TMPDIR=$(newdir "$build_dir/tmpdir")
+export TMPDIR
+testdir=$(newdir "$build_dir/test")
 
 cd "$testdir" 2>/dev/null || die "cd to $testdir failed"
 
@@ -318,10 +422,9 @@ export LIBPATH=/usr/lib:$LIBPATH
 echo -ne "\nSanity test archive\n\n"
 
 mkdir -p go/go-build-zos/
-cp "${build_dir}/build/${sanity_script_path}" go/go-build-zos/quicksanity.mak || die "failed to copy quicksanity.mak"
+cp "${repo}/${sanity_script_path}" go/go-build-zos/quicksanity.mak || die "failed to copy quicksanity.mak"
 
-"$CLEANUP" "/tmp" "$build_dir/build"
-rm -rf "${build_dir}/build"
+"$CLEANUP" "/tmp" "$build_dir"
 mkdir -p runtest
 cd runtest || die "cd failed"
 
@@ -336,15 +439,15 @@ go clean
 go clean -cache
 go clean -modcache
 
-export PATH="${build_dir}/go/${zos_util_dir}/bin:${PATH}"
+export PATH="${build_dir}/${repo}/${zos_util_dir}/bin:${PATH}"
 # retry the fail ones a couple of times
 retries=5
 if [[ "$SKIPTEST" == "1" ]]; then
         rc=0
         true
 else
+        echo "Starting Quicksanity. This might take a while..."
         storeenv >make.env
-        echo "Starting Quicksanity. This may take a while..."
         timeout 7200 make -k -j 20 -f "../go/go-build-zos/quicksanity.mak" > /dev/null 2>&1
         while [[ $? -ne 0 ]] && [[ $retries -gt 0 ]]; do
                 echo "Retrying Tests..."
@@ -355,11 +458,11 @@ else
                 #-------------------------
                 RC=$?
                 while [[ $INSPECT ]] && [[ "$(type -p storeenv)" != "" ]] && [[ $RC -ne 0 ]]; do
-                       echo make -k -f "../go/go-build-zos/quicksanity.mak" >>make.env
-                       echo Wait for 3600 second
-                       echo in $(pwd)/make.env
-                       sleep 3600
-                       [[ -r .env ]] && . ./.env
+                        echo make -k -f "../go/go-build-zos/quicksanity.mak" >>make.env
+                        echo Wait for 3600 second
+                        echo in $(pwd)/make.env
+                        sleep 3600
+                        [[ -r .env ]] && . ./.env
                 done
                 if [[ $RC -eq 0 ]]; then
                         true
@@ -372,29 +475,29 @@ else
 
 fi
 
-# Pretty output for test results
-~/parseGoTest "."
+# Test output formatting
+~/parseGoTest .
 
 rc=$?
 fail=0
 for f in $(ls *.tmp 2>/dev/null); do
         [[ $fail -eq 0 ]] && fail=1
-        #echo $f
-        #echo "=========================="
-        #/bin/grep FAIL $f
-        #if [[ $? -ne 0 ]]; then
-        #        /bin/cat $f
-        #fi
+#       echo $f
+#       echo "=========================="
+#       /bin/grep FAIL $f
+#       if [[ $? -ne 0 ]]; then
+#               /bin/cat $f
+#       fi
 done
 if [[ $fail -eq 0 ]] && [[ $rc -eq 0 ]] && [[ $do_upload -eq 1 ]]; then
         chmod go+r "${paxpath}"
         chmod go+r "${paxpath}.ilan"
-        echo "Artifactory Upload: ${paxpath} & ${paxpath}.ilan"
         /home/opnzos/local/bin/go-artifact -i "${paxpath}" -v -c 2>&1 | cat
         /home/opnzos/local/bin/go-artifact -i "${paxpath}.ilan" -v -c 2>&1 | cat
         rc=0
 fi
 disown $(jobs -p) 2>/dev/null
+#echo "Number of failures: $fail"
 echo "Test directory $(pwd) on $(hostname)"
 
 if [[ $fail -eq 0 ]] && [[ $rc -eq 0 ]]; then
